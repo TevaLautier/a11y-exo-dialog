1070fbffd1afb005e6414979eedddc01b8fc59fd
TevaLautier <teva.lautier@gmail.com>
2026-02-13 12:32:40 +0100

    fix: correction

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { A11yModule } from '@angular/cdk/a11y';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
@@ -5,17 +6,20 @@ import { bootstrapApplication } from '@angular/platform-browser';
 @Component({
   selector: 'app-root',
   templateUrl: './main.html',
-  imports:[CommonModule]
+  imports: [CommonModule, A11yModule]
 })
 export class App {
   name = 'Angular';
-  isOpen=false;
-  save(){}
-  closeModal(){
-    this.isOpen=false;
+  isOpen = false;
+  save() { }
+  
+  closeModal() {
+    this.isOpen = false;
+    document.getElementById('dialog-btn')?.focus()
   }
-  openModal(){
-    this.isOpen=true;
+  openModal() {
+    this.isOpen = true;
+    setTimeout(() => document.getElementById('dialog-close')?.focus())
   }
 
 }
